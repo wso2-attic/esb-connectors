d714ec28777d33d6cea4966034069cef6faf8f9e
vanjikumaran <vanjikumaran@gmail.com>
2015-03-03 18:49:39 +0530

    Added Signature algo

---
+++ Acquia/org.wso2.carbon.connector.acquia.contextdb/src/main/java/org/wso2/carbon/connector/authentication/GenerateSignature.java
@@ -0,0 +1,102 @@
+/*
+ *
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ * /
+ */
+
+package org.wso2.carbon.connector.authentication;
+
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.mediators.AbstractMediator;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class GenerateSignature extends AbstractMediator {
+
+    public static final String API_URI = "acquia.contextdb.apiUri";
+    public static final String SECRET_KEY = "acquia.contextdb.secret.key";
+    public static final String ACCESS_KEY = "acquia.contextdb.access.key";
+    public static final String HTTP_METHOD = "acquia.contextdb.httpMethod";
+    public static final String URL_PARAMETERS = "acquia.contextdb.parameters";
+    private static final String UTF8 = "UTF-8";
+    private static final String SIGNATURE = "acquia.contextdb.signature";
+
+    public void generateSignature(MessageContext msgctx) throws UnsupportedEncodingException,
+            NoSuchAlgorithmException, InvalidKeyException {
+
+        String secreteKey = msgctx.getProperty(SECRET_KEY).toString();
+        String accessKey = msgctx.getProperty(ACCESS_KEY).toString();
+        String httpMethod = msgctx.getProperty(HTTP_METHOD).toString();
+        String apiUri = msgctx.getProperty(API_URI).toString();
+        String queryParameters = msgctx.getProperty(URL_PARAMETERS).toString();
+
+        String baseString = calculateMessage(httpMethod, "", apiUri, queryParameters);
+
+        if (baseString != "" && accessKey != null && accessKey != null) {
+            //Create the HMAC signed Message
+            String singedMessage = "HMAC " + accessKey + ":" + HMACAuthenticationUtil.calculateRFC2104HMAC(baseString, secreteKey);
+            //Add the signature into the synapse property file
+            msgctx.setProperty(SIGNATURE, singedMessage);
+        } else {
+            msgctx.setProperty(SIGNATURE, "");
+        }
+
+
+    }
+
+    public boolean mediate(MessageContext messageContext) {
+        return false;
+    }
+
+
+    private String calculateMessage(String httpMethod, String headers, String apiUri, String queryParameters) throws UnsupportedEncodingException {
+
+
+        if (httpMethod != null && headers != null && apiUri != null && queryParameters != null) {
+            StringBuilder baseString = new StringBuilder();
+            //Added HTTP method in the first Line
+            baseString.append(httpMethod);
+            baseString.append("\n");
+            //TODO: Need to consider the Header
+
+            // add the URI
+            baseString.append(URLEncoder.encode(apiUri, UTF8));
+            //Add the Parameters
+            if (queryParameters != null && queryParameters.trim().length() > 0) {
+                List<String> nameValuePairs = Arrays.<String>asList(queryParameters.split("&"));
+                if (nameValuePairs.size() > 0) {
+                    Collections.sort(nameValuePairs);
+                    baseString.append("?").append(nameValuePairs.get(0));
+                    for (int i = 1; i < nameValuePairs.size(); i++) {
+                        baseString.append("&").append(nameValuePairs.get(i));
+                    }
+                }
+            }
+            return baseString.toString();
+        } else {
+            return "";
+        }
+
+    }
+
+}
---
+++ Acquia/org.wso2.carbon.connector.acquia.contextdb/src/main/java/org/wso2/carbon/connector/authentication/HMACAuthenticationUtil.java
@@ -0,0 +1,50 @@
+/*
+ *
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ * /
+ */
+
+package org.wso2.carbon.connector.authentication;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+
+public class HMACAuthenticationUtil {
+
+    private static final String UTF8 = "UTF-8";
+    private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+
+
+
+    public static String calculateRFC2104HMAC(String data, String key)throws UnsupportedEncodingException,
+            NoSuchAlgorithmException, InvalidKeyException {
+            SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(UTF8), HMAC_SHA1_ALGORITHM);
+            Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+            mac.init(signingKey);
+            byte[] rawHmac = mac.doFinal(data.getBytes());
+            return Base64.encodeBase64String(rawHmac);
+    }
+
+
+
+}
