285216938f689a6dee28f34188a432691905e0c7
vanjikumaran <vanjikumaran@gmail.com>
2015-03-12 14:02:54 +0530

    added JsonUtil

---
+++ Acquia/org.wso2.carbon.connector.acquia.contextdb/src/main/java/org/wso2/carbon/connector/utils/JsonUtil.java
@@ -0,0 +1,65 @@
+/*
+ *
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ * /
+ */
+
+package org.wso2.carbon.connector.utils;
+
+
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.SynapseException;
+import org.apache.synapse.mediators.AbstractMediator;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+
+public class JsonUtil extends AbstractMediator {
+
+    public static final String PAYLOAD = "acquia.contextdb.payload";
+
+    public boolean mediate(MessageContext messageContext) {
+
+        String payload = messageContext.getProperty(PAYLOAD).toString();
+
+        JSONArray jsonArray = null;
+        StringBuilder basestring = null;
+        try {
+            if (payload!=null) {
+                jsonArray = new JSONArray(payload);
+                for (int i = 0, size = jsonArray.length(); i < size; i++) {
+                    JSONObject objectInArray = jsonArray.getJSONObject(i);
+                    if (basestring == null) {
+                        basestring = new StringBuilder(objectInArray.toString());
+                    } else {
+                        basestring.append("\n");
+                        basestring.append(objectInArray.toString());
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            throw new SynapseException(e);
+        }
+        if (basestring!=null) {
+            messageContext.setProperty(PAYLOAD,basestring.toString());
+        }
+        return true;
+
+    }
+
+
+}
