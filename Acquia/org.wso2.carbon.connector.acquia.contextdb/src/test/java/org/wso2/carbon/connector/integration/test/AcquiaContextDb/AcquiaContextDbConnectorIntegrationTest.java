6ed3b5077dd85175cc78db5e27536b80e7401cd2
elilmatha <elilsiva3@gmail.com>
2015-03-23 12:14:25 +0530

    Update AcquiaContextDbConnectorIntegrationTest.java

--- Acquia/org.wso2.carbon.connector.acquia.contextdb/src/test/java/org/wso2/carbon/connector/integration/test/AcquiaContextDb/AcquiaContextDbConnectorIntegrationTest.java
+++ Acquia/org.wso2.carbon.connector.acquia.contextdb/src/test/java/org/wso2/carbon/connector/integration/test/AcquiaContextDb/AcquiaContextDbConnectorIntegrationTest.java
@@ -104,7 +104,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
      *
      * Positive test case for getAllSegmants method.
      */
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {getAllSegmants} integration test with positive case.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {getAllSegmants} integration test with positive case.")
     public void getAllSegmants() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_getAllSegments.txt";
         String methodName = "AcquiaContextDb_getAllSegments";
@@ -123,7 +123,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Negative test case for getAllSegmentsNegativeCase method with negative parameters.
      */
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {getAllSegmentsNegativeCase} integration test with negative parameter.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {getAllSegmentsNegativeCase} integration test with negative parameter.")
     public void getAllSegmantsNegativeCase() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_getAllSegmentsNegativeCase.txt";
         String methodName = "AcquiaContextDb_getAllSegments";
@@ -145,7 +145,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
      *
      * Positive test case for deteteEvent method.
      */
-    @Test(enabled=false,groups = {"wso2.esb"}, description = "AcquiaContextDb {deteteEvent} ")
+    @Test(enabled=true,groups = {"wso2.esb"}, description = "AcquiaContextDb {deteteEvent} ")
     public void deteteEvent() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_deleteEvent.txt";
         String methodName = "AcquiaContextDb_deteteEvent";
@@ -164,7 +164,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Negative test case for deteteEvent method with negative parameters.
      */
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {deleteEventNegativeCase} integration test with negative parameter.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {deleteEventNegativeCase} integration test with negative parameter.")
     public void deleteEventNegativeCaseNegativeCase() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_deleteEventNegativeCase.txt";
         String methodName = "AcquiaContextDb_deteteEvent";
@@ -185,7 +185,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
      *
      * Positive test case for exportVisitorData method with mendatory parameters.
      */
-    @Test(enabled=false,groups = {"wso2.esb"}, description = "AcquiaContextDb {exportVisitorData} ")
+    @Test(enabled=true,groups = {"wso2.esb"}, description = "AcquiaContextDb {exportVisitorData} ")
     public void exportVisitorData() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_exportVisitorData.txt";
         String methodName = "AcquiaContextDb_exportVisitorData";
@@ -206,7 +206,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
      *
      * Positive test case for exportVisitorData  with optional parameters.
      */
-    @Test(enabled=false,groups = {"wso2.esb"}, description = "AcquiaContextDb {exportVisitorDataIOptional} ")
+    @Test(enabled=true,groups = {"wso2.esb"}, description = "AcquiaContextDb {exportVisitorDataIOptional} ")
     public void exportVisitorDataOptional() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_exportVisitorDataIOptional.txt";
         String methodName = "AcquiaContextDb_exportVisitorData";
@@ -227,7 +227,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Negative test case for exportVisitorData method with negative parameters.
      */
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {exportVisitorDataNegativeCase} integration test with negative parameter.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {exportVisitorDataNegativeCase} integration test with negative parameter.")
     public void exportVisitorDataNegativeCase() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_exportVisitorDataNegativeCase.txt";
         String methodName = "AcquiaContextDb_exportVisitorData";
@@ -248,7 +248,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
      *
      * Positive test case for createEvent method.
      */
-    @Test(enabled=false,groups = {"wso2.esb"}, description = "AcquiaContextDb {createEvent} ")
+    @Test(enabled=true,groups = {"wso2.esb"}, description = "AcquiaContextDb {createEvent} ")
     public void createEvent() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_createEvent.txt";
         String methodName = "AcquiaContextDb_createEvent";
@@ -269,7 +269,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      Negative test case for createEvent method with negative parameters.
      **/
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {createEventNegativeCase} integration test with negative parameter.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {createEventNegativeCase} integration test with negative parameter.")
     public void createEventNegativeCase() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_createEventNegativeCase.txt";
         String methodName = "AcquiaContextDb_createEvent";
@@ -290,7 +290,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
      *
      * Positive test case for getVisitorQuery method.
      */
-    @Test(enabled=false,groups = {"wso2.esb"}, description = "AcquiaContextDb {getVisitorQuery} ")
+    @Test(enabled=true,groups = {"wso2.esb"}, description = "AcquiaContextDb {getVisitorQuery} ")
     public void getVisitorQuery() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_getVisitorQuery.txt";
         String methodName = "AcquiaContextDb_getVisitorQuery";
@@ -311,7 +311,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Negative test case for getVisitorQuery method with negative parameters.
      */
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {getVisitorQueryNegativeCase} integration test with negative parameter.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {getVisitorQueryNegativeCase} integration test with negative parameter.")
     public void getVisitorQueryNegativeCase() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_getVisitorQueryNegativeCase.txt";
         String methodName = "AcquiaContextDb_getVisitorQuery";
@@ -332,7 +332,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
      *
      * Positive test case for getExportVisitorDataStatus method.
      */
-    @Test(enabled=false,groups = {"wso2.esb"}, description = "AcquiaContextDb {getExportVisitorDataStatus} ")
+    @Test(enabled=true,groups = {"wso2.esb"}, description = "AcquiaContextDb {getExportVisitorDataStatus} ")
     public void getExportVisitorDataStatus() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_getExportVisitorDataStatus.txt";
         String methodName = "AcquiaContextDb_getExportVisitorDataStatus";
@@ -352,7 +352,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Negative test case for getExportVisitorDataStatus method with negative parameters.
      */
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {getExportVisitorDataStatusNegativeCase} integration test with negative parameter.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {getExportVisitorDataStatusNegativeCase} integration test with negative parameter.")
     public void getExportVisitorDataStatusNegativeCase() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_getExportVisitorDataStatusNegativeCase.txt";
         String methodName = "AcquiaContextDb_getExportVisitorDataStatus";
@@ -372,7 +372,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Positive test case for getSegmentsById method.
      */
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {getSegmentsById} integration test with positive case.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {getSegmentsById} integration test with positive case.")
     public void getSegmentsById() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_getSegmentsById.txt";
         String methodName = "AcquiaContextDb_getSegmentsById";
@@ -391,7 +391,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Negative test case for getSegmentsById method with negative parameters.
      */
-    @Test(enabled=false, groups = {"wso2.esb"}, description = "AcquiaContextDb {getSegmentsByIdNegativeCase} integration test with negative parameter.")
+    @Test(enabled=true, groups = {"wso2.esb"}, description = "AcquiaContextDb {getSegmentsByIdNegativeCase} integration test with negative parameter.")
     public void getSegmentsByIdNegativeCase() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_getSegmentsByIdNegativeCase.txt";
         String methodName = "AcquiaContextDb_getSegmentsById";
@@ -410,7 +410,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Positive test case for importEvent method.
      */
-    @Test(enabled=false,groups = {"wso2.esb"}, description = "AcquiaContextDb {importEvent} ")
+    @Test(enabled=true,groups = {"wso2.esb"}, description = "AcquiaContextDb {importEvent} ")
     public void importEvent() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_importEvent.txt";
         String methodName = "AcquiaContextDb_importEvent";
@@ -430,7 +430,7 @@ public class AcquiaContextDbConnectorIntegrationTest extends ESBIntegrationTest 
     /**
      * Positive test case for importEvent method with multiple entry parameters.
      */
-    @Test(enabled=false,groups = {"wso2.esb"}, description = "AcquiaContextDb {importEvent} ")
+    @Test(enabled=true,groups = {"wso2.esb"}, description = "AcquiaContextDb {importEvent} ")
     public void importEventMandatory() throws Exception {
         String jsonRequestFilePath = pathToRequestsDirectory + "AcquiaContextDb_importEventOptional.txt";
         String methodName = "AcquiaContextDb_importEvent";
