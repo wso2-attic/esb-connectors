f85268fb25b924ff57f13a05d1f99f1451692e79
Shaki <Shaki@Shakis-MacBook-Pro.local>
2015-10-02 10:40:37 +0530

    Use constant instead of harcoding value

--- amazonsqs/amazonsqs-connector/amazonsqs-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/amazonsqs/auth/AmazonSQSContext.java
+++ amazonsqs/amazonsqs-connector/amazonsqs-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/amazonsqs/auth/AmazonSQSContext.java
@@ -48,7 +48,7 @@ public class AmazonSQSContext extends AbstractConnector {
         log.debug("Removing the unneeded properties of the already run methods");
         Object[] keys = messageContext.getPropertyKeySet().toArray();
         for (Object key : keys) {
-            if ((key).toString().startsWith("uri.var.") && !(key).toString().startsWith(AmazonSQSConstants.API_URL)
+            if ((key).toString().startsWith(AmazonSQSConstants.URI_VAR_PREFIX) && !(key).toString().startsWith(AmazonSQSConstants.API_URL)
                     && !(key).toString().startsWith(AmazonSQSConstants.ACCESS_KEY_ID) && !(key).toString().startsWith(AmazonSQSConstants.SECRET_ACCESS_KEY)
                     && !(key).toString().startsWith(AmazonSQSConstants.CONTENT_TYPE) && !(key).toString().startsWith(AmazonSQSConstants.SERVICE)
                     && !(key).toString().startsWith(AmazonSQSConstants.SIGNATURE_VERSION) && !(key).toString().startsWith(AmazonSQSConstants.SIGNATURE_METHOD)
