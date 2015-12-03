8ceaa8629e7411cb596f6fddf6865e77b5ff0c7b
Rajjaz <hmrajas1990@gmail.com>
2015-12-03 14:08:42 +0530

    Change directory name

---
+++ feed/feed-connector/feed-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/FeedConstant.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.carbon.connector;
+
+public class FeedConstant {
+    public  static final String hostAddress="HostAddress";
+    public  static final String title="Title";
+    public  static final String content="Content";
+    public  static final String author="Author";
+    public  static final String feedID="FeedID";
+    public  static final String contentType="application/atom+xml;type=entry";
+    public  static final String Result="Result";
+    public  static final String Status="status";
+    public  static final String result="result";
+    public  static final String entryID="EntryID";
+    public  static final String ns="ns";
+}
